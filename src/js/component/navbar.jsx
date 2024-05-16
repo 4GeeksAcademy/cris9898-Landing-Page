bf847ed5bbecf8d85594c7377c1607b1e47ed4e5
Cris9898 <157153132+Cris9898@users.noreply.github.com>
2024-05-16 22:58:43 +0000

    Second Commit

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -5,14 +5,14 @@ const navBar = () =>{
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fs-6">
                 <div className="container">
-                    <div className="d-flex justify-content-end">
+                    
                         <a className="navbar-brand " href="#">Start Bootstrap</a>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
-                    </div>
-                <div className="collapse navbar-collapse m-2 d-flex justify-content-end" id="navbarNav">
-                    <ul className="navbar-nav">
+                    
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
                             <a className="nav-link" href="#">Home</a>
                         </li>
